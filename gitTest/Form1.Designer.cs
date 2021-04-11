f49035c4bfcd4f095565bbb714d993d832c9bea8
GyHalmi <georgehalmi@gmail.com>
2021-04-11 09:52:38 +0200

    test cmd commit

--- gitTest/Form1.Designer.cs
+++ gitTest/Form1.Designer.cs
@@ -38,8 +38,9 @@ namespace gitTest
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "pontos idő";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
